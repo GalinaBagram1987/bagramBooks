50f402cbf1773769b2bd9a488b1be1a888070eae
GalinaBagram1987 <gaby_ev@mail.ru>
2026-07-12 21:40:35 +1000

    added adress

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',  // для своего домена — корень
+  base: '/bagramBooks',  // для своего домена — корень
 });
