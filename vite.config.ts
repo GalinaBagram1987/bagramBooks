874bc917724131aa8bc7c7f80bce6f8c7ed7d192
GalinaBagram1987 <gaby_ev@mail.ru>
2026-07-12 22:13:07 +1000

    added sitemap, robot

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/bagramBooks',  // для своего домена — корень
+  base: '/',  // для своего домена — корень
 });
